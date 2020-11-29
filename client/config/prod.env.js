a12b74cb1c49cf38577a6dd243ba323cde85e4f6
Anton <anton@happyorder.io>
2020-11-29 15:12:54 +0100

    Now redy for the new  DynamoDB  api

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_BASE_URL: 'https://9ovaow6lq5.execute-api.eu-north-1.amazonaws.com/prod/'
+  API_BASE_URL: '"https://9ovaow6lq5.execute-api.eu-north-1.amazonaws.com/prod"'
 }
